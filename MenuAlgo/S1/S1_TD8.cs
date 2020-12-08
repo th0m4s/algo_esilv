7a9298c102efd38ed09af67d34e87ed0465d1298
Thomas LEDOS <thomas.ledos92@gmail.com>
2020-12-08 08:45:10 +0100

    Ajout du TD8 et des sous-exercices

---
+++ MenuAlgo/S1/S1_TD8.cs
@@ -0,0 +1,139 @@
+﻿using OutilsTD;
+using System;
+
+namespace MenuAlgo
+{
+    public class S1_TD8
+    {
+        [Exercice("Valeur absolue (sans Math.Abs())")]
+        static void Exercice1_1()
+        {
+            Console.WriteLine("Saisir un nombre :");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n < 0)
+                n = -n;
+
+            Console.WriteLine("La valeur absolue est " + n);
+        }
+
+        [Exercice("Liste des carrés < 100")]
+        static void Exercice2_2()
+        {
+            Console.WriteLine("Liste des carrés < 100");
+            int n = 1;
+            int carre = n * n;
+            while (carre < 100)
+            {
+                Console.WriteLine(n + " au carré = " + carre);
+                n = n + 1;
+                carre = n * n;
+            }
+        }
+
+        [Exercice("Liste des 9 premiers carrés")]
+        static void Exercice3_2()
+        {
+            Console.WriteLine("Liste des carrés");
+            for (int i = 1; i <= 9; i++)
+            {
+                int carre = i * i;
+                Console.WriteLine(i + " au carré = " + carre);
+            }
+        }
+
+        [Exercice("Factorielle")]
+        static void Exercice4_1()
+        {
+            Console.Write("Saisir un nombre : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+
+            for (int i = 2; i <= n; i++)
+                fact *= i;
+
+            Console.WriteLine("La factorielle de " + n + " est " + fact);
+        }
+
+        [Exercice("PGCD de 2 nombres")]
+        static void Exercice4_2()
+        {
+            Console.Write("Saisir un premier nombre : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Saisir un deuxième nombre : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int ca = a, cb = b, t = 0;
+            while (cb != 0)
+            {
+                t = cb;
+                cb = ca % cb;
+                ca = t;
+            }
+
+            Console.WriteLine("Le PGCD de " + a + " et de " + b + " est " + ca);
+        }
+
+        [Exercice("Division euclidienne")]
+        static void Exercice4_3()
+        {
+            Console.Write("Saisir un premier nombre : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Saisir un deuxième nombre : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            // Version rapide :)
+            /*int q = a/b;
+            int r = a - q*b;*/
+
+
+            int ca = a, q = 0;
+            while (ca > b)
+            {
+                ca -= b;
+                q++;
+            }
+            int r = ca;
+
+
+            Console.WriteLine("Pour la division euclidienne de " + a + " par " + b + ", q = " + q + " et r = " + r);
+        }
+
+        [Exercice("Test de primarité d'un nombre")]
+        static void Exercice4_4()
+        {
+            Console.Write("Saisir un nombre : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            bool premier = true;
+            // vu qu'il y a plein de cas possibles et que c'est mieux d'afficher à un seul endroit,
+            // je stocke dans une variable la primarité de n
+
+            if (n > 2)
+            { // 2 est un cas spécial, c'est premier, donc je regarde d'abord si n > 2
+              // ensuite je regarde si n est divisible par un nombre plus petit que n mais plus
+              // grand que 1
+                for (int i = 2; i <= Math.Floor(Math.Sqrt(n)); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        premier = false;
+                        // break sort du for, on sait que le nombre n'est pas premier
+                        // on aurait pu faire un WriteLine puis un return pous arrêter la fonction
+                        break;
+                    }
+                }
+            }
+            else if (n <= 1) premier = false;
+            // si n == 2, premier reste à true, mais si n vaut 1, 0, ou si n est négatif,
+            // alors ce n'est pas un nombre premier
+
+            if (premier)
+                Console.WriteLine("Le nombre " + n + " est premier");
+            else Console.WriteLine("Le nombre " + n + " n'est pas premier");
+        }
+    }
+}
--- OutilsTD/GestionTD.cs
+++ OutilsTD/GestionTD.cs
@@ -10,27 +10,29 @@ namespace OutilsTD
     {
         Semestre semestre;
         int numeroTD;
-        Dictionary<int, KeyValuePair<string, Action>> exercices = new Dictionary<int, KeyValuePair<string, Action>>();
+        Dictionary<string, KeyValuePair<string, Action>> exercices = new Dictionary<string, KeyValuePair<string, Action>>();
 
         public GestionTD(Semestre semestre, int numeroTD, Type classeTD)
         {
             this.semestre = semestre;
             this.numeroTD = numeroTD;
 
-            string patternExo = @"^Exercice(?<n>\d+)$";
+            string patternExo = @"^Exercice(?<n>\d+)(_(?<s>\d+)){0,1}$";
             foreach (MethodInfo methodInfo in classeTD.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
             {
                 Match match = Regex.Match(methodInfo.Name, patternExo);
                 if (match.Success)
                 {
-                    int numExo = int.Parse(match.Groups["n"].Value);
+                    string keyExo = match.Groups["n"].Value;
+                    string subKey = match.Groups["s"].Value;
+                    if(subKey != null && subKey.Length > 0) keyExo += "-" + subKey;
 
                     string nomExercice = "";
                     List<ExerciceAttribute> exerciceAttributes = new List<ExerciceAttribute>(methodInfo.GetCustomAttributes<ExerciceAttribute>());
                     if (exerciceAttributes.Count > 0)
                         nomExercice = exerciceAttributes[0].nomExercice;
 
-                    exercices.Add(numExo, new KeyValuePair<string, Action>(nomExercice, () =>
+                    exercices.Add(keyExo, new KeyValuePair<string, Action>(nomExercice, () =>
                     {
                         methodInfo.Invoke(null, null);
                     }));
@@ -48,15 +50,15 @@ namespace OutilsTD
 
                 while(continuer == 'r')
                 {
-                    int numeroExercice = DemanderExercice();
-                    if (numeroExercice > 0)
+                    string numeroExercice = DemanderExercice();
+                    if (numeroExercice != null && numeroExercice.Length > 0)
                     {
                         ExecuterExercice(numeroExercice);
 
                         Console.WriteLine("\nEntre 'r' pour revenir au TD, 'q' pour quitter ou n'importe quelle touche pour revenir au menu principal...");
                         continuer = Console.ReadKey(true).KeyChar;
                     }
-                    else if (numeroExercice == 0)
+                    else if (numeroExercice == "")
                         continuer = 'm'; // ou n'importe quoi, on veut juste revenir au menu, donc il faut quelque chose différent de r et q
                     else continuer = 'q';
                 }
@@ -67,29 +69,38 @@ namespace OutilsTD
             return true;
         }
 
-        public int DemanderExercice()
+        public string DemanderExercice()
         {
             Console.WriteLine("Liste des exercices disponibles pour le TD" + numeroTD + " (" + semestre.Display() + ") :");
-            foreach(KeyValuePair<int, KeyValuePair<string, Action>> exercice in exercices)
+            foreach(KeyValuePair<string, KeyValuePair<string, Action>> exercice in exercices)
             {
                 Console.WriteLine(" - Exercice " + exercice.Key + (exercice.Value.Key.Length > 0 ? " : " + exercice.Value.Key + "" : ""));
             }
 
             Console.WriteLine(""); // marge
 
-            int numeroChoisi, max = exercices.Count;
+            string keyChoisie = "";
             do
             {
                 Console.Write("Entre un numéro d'exercice, 0 pour revenir au menu principal et 'q' pour quitter : ");
                 string ligne = Console.ReadLine();
-                if (ligne == "q") return -1;
-                numeroChoisi = int.Parse(ligne);
-            } while (numeroChoisi <= 0 || numeroChoisi > max);
+                if (ligne == "q") return null;
+                if (ligne == "0") return "";
 
-            return numeroChoisi;
+                string[] parts = ligne.Split('-', '_', '.', ',');
+                if(parts.Length == 2)
+                {
+                    if (int.TryParse(parts[0], out var p1) && int.TryParse(parts[1], out var p2)) keyChoisie = string.Join("-", parts);
+                } else if(parts.Length == 1)
+                {
+                    if (int.TryParse(parts[0], out var p1)) keyChoisie = parts[0].ToString();
+                }
+            } while (!exercices.ContainsKey(keyChoisie));
+
+            return keyChoisie;
         }
 
-        public void ExecuterExercice(int numeroExercice)
+        public void ExecuterExercice(string numeroExercice)
         {
             if (exercices.ContainsKey(numeroExercice))
             {
